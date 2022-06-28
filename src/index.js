fa756c8046eebf980b2d63fa060cba924eb14b8b
Ackheron <ackheron@etik.com>
2022-06-28 16:31:02 +0200

    composant initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <section id="todo">
+        <h1 className="m-3">Liste de tâches</h1>
+        <ul className="list-group m-3">
+          <li className="list-group-item d-flex align-tiems-center">
+            Ranger la vaisselle
+            <button className="btn btn-sm ml-auto btn-outline-success">
+              &#x2713;
+            </button>
+          </li>
+          <li className="list-group-item d-flex align-tiems-center">
+            Répondre appel d'offres
+            <button className="btn btn-sm ml-auto btn-outline-success">
+              &#x2713;
+            </button>
+          </li>
+          <li className="list-group-item d-flex align-tiems-center">
+            Signer contrat
+            <button className="btn btn-sm ml-auto btn-outline-success">
+              &#x2713;
+            </button>
+          </li>
+          <li className="list-group-item d-flex align-tiems-center">
+            Ranger la salon
+            <button className="btn btn-sm ml-auto btn-outline-success">
+              &#x2713;
+            </button>
+          </li>
+        </ul>
+        <footer
+          className="d-flex justify-content-between bg-secondary p-3"
+          id="mainFooter"
+        >
+          <div className="btn-group">
+            <a href="#" className="btn btn-outline-dark bg-light">
+              List
+            </a>
+            <a href="#" className="btn btn-outline-dark bg-light">
+              Completed
+            </a>
+            <a href="#" className="btn btn-outline-dark bg-light">
+              Add
+            </a>
+          </div>
+        </footer>
+      </section>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
