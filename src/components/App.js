177f6b5b9207b9e4adc20a3853465a2dae6a835e
Ackheron <ackheron@etik.com>
2022-07-25 12:09:20 +0200

    class component to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -5,36 +5,36 @@ import AddTask from "./AddTask";
 import initialData from "../initialData";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// const App = () => {
-//   return (
-//     <section id="todo">
-//       <Router>
-//         <Switch>
-//           <Route path="/add-task" component={AddTask} />
+const App = () => {
+  return (
+    <section id="todo">
+      <Router>
+        <Switch>
+          <Route path="/add-task" component={AddTask} />
 
-//           <Route path="/" component={ToDoList} />
-//         </Switch>
-//         <Footer />
-//       </Router>
-//     </section>
-//   );
-// };
+          <Route path="/" component={ToDoList} />
+        </Switch>
+        <Footer />
+      </Router>
+    </section>
+  );
+};
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <section id="todo">
-        <Router>
-          <Switch>
-            <Route path="/add-task" component={AddTask} />
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <section id="todo">
+//         <Router>
+//           <Switch>
+//             <Route path="/add-task" component={AddTask} />
 
-            <Route path="/" component={ToDoList} />
-          </Switch>
-          <Footer />
-        </Router>
-      </section>
-    );
-  }
-}
+//             <Route path="/" component={ToDoList} />
+//           </Switch>
+//           <Footer />
+//         </Router>
+//       </section>
+//     );
+//   }
+// }
 
-// export default App;
+export default App;
