326b9e520ab8d3915a971c6ec7142d9be1524e28
Ackheron <ackheron@etik.com>
2022-07-26 11:57:57 +0200

    State completed du composant

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,31 @@
 import React from "react";
 
 class ToDo extends React.Component {
+  state = {
+    completed: this.props.task.completed,
+  };
+
+  toggleCompleted = () => {
+    this.setState((prevState) => ({
+      completed: !prevState.completed,
+    }));
+  };
   render() {
     return (
-      <li className="list-group-item d-flex justify-content-between align-items-center">
+      <li
+        className={
+          "list-group-item d-flex justify-content-between align-items-center " +
+          (this.state.completed ? "bg-succes" : null)
+        }
+      >
         {this.props.task.name}
-        <button className="btn btn-sm ml-auto btn-outline-success">
+        <button
+          className={
+            "btn btn-sm ml-auto " +
+            (this.state.completed ? "btn-success" : "btn-ouline-success")
+          }
+          onClick={() => this.toggleCompleted()}
+        >
           &#x2713;
         </button>
       </li>
