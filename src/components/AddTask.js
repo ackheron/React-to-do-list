6733ecace04139b697aaec8c925a82bb1f0dfcd1
Ackheron <ackheron@etik.com>
2022-08-15 11:15:33 +0200

    Ref AddTasks

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 class AddTask extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.newTask.value);
+  };
+
   render() {
     return (
       <section>
